7e497c22d447e7452f413db729313d6fcd5d2f70
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-23 12:11:12 +0300

    interview-public-1 Task 1: Added lombok Updated current implementation of AccountServiceImpl

--- src/main/java/com/devexperts/account/Account.java
+++ src/main/java/com/devexperts/account/Account.java
@@ -1,35 +1,21 @@
 package com.devexperts.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Builder
 public class Account {
+
     private final AccountKey accountKey;
+
     private final String firstName;
+
     private final String lastName;
+
     private Double balance;
-
-    public Account(AccountKey accountKey, String firstName, String lastName, Double balance) {
-        this.accountKey = accountKey;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.balance = balance;
-    }
-
-    public AccountKey getAccountKey() {
-        return accountKey;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
 }
--- src/main/java/com/devexperts/account/AccountKey.java
+++ src/main/java/com/devexperts/account/AccountKey.java
@@ -1,18 +1,25 @@
 package com.devexperts.account;
 
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Unique Account identifier
  *
  * <p>
  * NOTE: we suspect that later {@link #accountId} is not going to be uniquely identifying an account,
  * as we might add human-readable account representation and some clearing codes for partners.
+ * </p>
+ * todo: on accountId business logic change, make real unique account identifier and
+ *  switch {@link EqualsAndHashCode.Include} to it.
  * */
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class AccountKey {
-    private final long accountId;
 
-    private AccountKey(long accountId) {
-        this.accountId = accountId;
-    }
+    @EqualsAndHashCode.Include
+    private final long accountId;
 
     public static AccountKey valueOf(long accountId) {
         return new AccountKey(accountId);
